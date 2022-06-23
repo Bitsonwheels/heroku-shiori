09b1d0b86bd9ddb2dc977f5a60a0b1d4b0ab1ac9
Bitsonwheels <51041273+Bitsonwheels@users.noreply.github.com>
2022-06-23 03:21:23 +0200

    Update root.go

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -92,7 +92,6 @@ func getDataDir(portableMode bool) (string, error) {
 
 	// Try to use platform specific app path
 	userScope := apppaths.NewScope(apppaths.User, "shiori")
-	// dataDir, err := userScope.DataPath("")
 	dataDir, err := userScope.DataPath("shiori.db")
 	if err == nil {
 		return dataDir, nil
